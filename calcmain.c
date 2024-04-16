43de1f2ded1fcdc3d9f90b7d2b57839f6ff59705
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2024-04-16 17:25:54 +0530

    first build

---
+++ calcmain.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,op;
+    printf("this is a simple c integer calculator\ninput a number:");
+    scanf("%d",&num1);
+    printf("\ninput another number:");
+    scanf("%d",&num2);
+    printf("\nenter 1 for add\nenter 2 for minus\nenter 3 for mult\nenter 4 for div\n");
+    scanf("%d",&op);
+    switch(op)
+    {
+        case 1:printf("%d",num1+num2);
+        break;
+        case 2:printf("%d",num1-num2);
+        break;
+        case 3:printf("%d",num1*num2);
+        break;
+        case 4:printf("%d",num1/num2);
+        break;
+        default:printf("invalid option");
+    }
+}
